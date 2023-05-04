36488282cb7c5dadaaeed7a9396431f5e04cdaea
myilokka <rumpeldumpel03@gmail.com>
2023-05-04 19:12:39 +0800

    linter

--- logistic/views.py
+++ logistic/views.py
@@ -3,7 +3,8 @@ from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination,\
+    LimitOffsetPagination
 
 
 class ProductViewSet(ModelViewSet):
@@ -13,6 +14,7 @@ class ProductViewSet(ModelViewSet):
     search_fields = ['title', 'description']
     pagination_class = PageNumberPagination
 
+
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
@@ -20,5 +22,3 @@ class StockViewSet(ModelViewSet):
     filterset_fields = ['products']
     search_fields = ['products__title']
     pagination_class = LimitOffsetPagination
-
-
